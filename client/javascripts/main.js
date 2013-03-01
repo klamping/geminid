09c498b74fa3845e05669e9524da155b8f92499d
Kevin Lamping <kevin.lamping@usaa.com>
2013-02-28 21:02:14 -0600

    added simple status indicator for server availability

--- client/javascripts/main.js
+++ client/javascripts/main.js
@@ -47,6 +47,22 @@ $(document).idleTimer();
 $(document).on("active.idleTimer", function () { setActive(true); });
 $(document).on("idle.idleTimer", function () { setActive(false); });
 
+Template.loginPanel.meteor_status = function () {
+    var icon;
+    switch (Meteor.status().status) {
+        case 'connecting':
+            icon = "refresh";
+            break;
+        case 'connected':
+            icon = "ok";
+            break;
+        case 'disconnected':
+        default:
+            icon = "remove";
+    }
+    return icon;
+};
+
 Template.loginPanel.loggedOut = function () {
     return Meteor.user() === null ? "logged-out" : "";
 };
