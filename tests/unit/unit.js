7a830beff8754bfda8dc677eac56615458ca6dc6
Kevin Lamping <klamping@gmail.com>
2013-03-02 20:45:25 -0600

    added tests

---
+++ tests/unit/unit.js
@@ -0,0 +1,11 @@
+var chai = require("chai");
+chai.
+
+describe('Geminid', function(){
+  describe('#setUnreadCount()', function(){
+    it('should something', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
