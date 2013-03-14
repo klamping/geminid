519b60f2a20dd07bf804e5ff7f0ace4e971ac4f3
Kevin Lamping <klamping@gmail.com>
2013-03-14 16:58:34 -0500

    adding comments for how to write casper tests

---
+++ tests/func/casper.js
@@ -0,0 +1,14 @@
+// Auto-scroll
+//  should scroll to bottom of messages on page load
+//  should auto-scroll when a new message is posted and user is at bottom of message list
+//  should not auto-scroll when user is not at bottom of message list
+
+// Missed message notification
+//  should add correct number of missed messages when user not focused on window
+//  should not have any missed messages when user focused on window
+
+
+// Idle/Away
+//  should show as idle after >60 seconds of inactivity
+//  should show as idle if screen isn't focused
+//  should show as offline if user isn't logged in
