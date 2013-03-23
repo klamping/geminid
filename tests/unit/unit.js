bc09abb9a7114353e39cae244475b845fb7c4f7e
Kevin Lamping <klamping@gmail.com>
2013-03-22 23:07:08 -0500

    got some tests going for formatting of links

--- tests/unit/unit.js
+++ tests/unit/unit.js
@@ -1,11 +1,19 @@
 var chai = require("chai");
-chai.
+chai.should();
+
+var utils = require("../../client/javascripts/message-formatting.js");
 
 describe('Geminid', function(){
-  describe('#setUnreadCount()', function(){
-    it('should something', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+  describe('#replaceURLWithHTMLLinks()', function(){
+    it('should replace a plain url with markdown format', function(){
+        utils.replaceURLWithHTMLLinks("http://www.url.com").should.equal('[http://www.url.com](http://www.url.com)');
+    });
+    it('should replace a plain url in text with an  HTML link', function(){
+        utils.replaceURLWithHTMLLinks("This http://www.url.com is my website").should.equal('This [http://www.url.com](http://www.url.com) is my website');
+    });
+    it('should not change Markdown links', function(){
+        var markdownLink = "This is [an example](http://example.com/) inline link.";
+        utils.replaceURLWithHTMLLinks(markdownLink).should.equal(markdownLink);
+    });
+  });
+});
