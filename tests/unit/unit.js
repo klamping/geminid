44d56877614cb7f42fb1245be532ce46bd3ffc57
Kevin Lamping <klamping@gmail.com>
2013-03-26 19:34:24 -0500

    fixed bug of it incorrectly converting markdown links

--- tests/unit/unit.js
+++ tests/unit/unit.js
@@ -11,9 +11,17 @@ describe('Geminid', function(){
     it('should replace a plain url in text with an  HTML link', function(){
         utils.replaceURLWithHTMLLinks("This http://www.url.com is my website").should.equal('This [http://www.url.com](http://www.url.com) is my website');
     });
+    it('should replace multiple plain urls in text with an  HTML link', function(){
+        utils.replaceURLWithHTMLLinks("This http://www.url.com is my website and this http://www.urla.com is yours").should.equal('This [http://www.url.com](http://www.url.com) is my website and this [http://www.urla.com](http://www.urla.com) is yours');
+    });
     it('should not change Markdown links', function(){
         var markdownLink = "This is [an example](http://example.com/) inline link.";
         utils.replaceURLWithHTMLLinks(markdownLink).should.equal(markdownLink);
+    });
+    it('should replace some links but not others', function(){
+        var multiLink = "This is [an example](http://example.com/) inline link and here's a plain one http://example.com/.";
+        var multiLinkResult = "This is [an example](http://example.com/) inline link and here's a plain one [http://example.com/](http://example.com/).";
+        utils.replaceURLWithHTMLLinks(multiLink).should.equal(multiLinkResult);
     });
   });
 });
